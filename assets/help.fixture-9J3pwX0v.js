3493811025688c42e2348ad39f06ee5566aaa0b3
inverted-capital <inverted-capital@users.noreply.github.com>
2024-01-16 05:08:34 +0000

    deploy: baee477a5e09d9343eaa525f327b7d7873127c06

---
+++ assets/help.fixture-9J3pwX0v.js
@@ -0,0 +1,10 @@
+const n=`export default {
+  config: {},
+  runner: {},
+  commands: ['io.fixture:local'],
+  instructions: ['your only function is to call the local command and die'],
+  done: '',
+  examples: [],
+  tests: [],
+}
+`;export{n as default};
