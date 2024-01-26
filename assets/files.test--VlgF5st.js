7612565d8008a3c8e6df6a6ca787dbfc895334ef
inverted-capital <inverted-capital@users.noreply.github.com>
2024-01-26 04:18:57 +0000

    deploy: 070a9555ddfcebbc4a487654dcb0f1bfa630598f

--- assets/files.test--VlgF5st.js
+++
@@ -1,1 +0,0 @@
-import{h as l,d as s}from"./test-context-ue4e_8w-.js";import"./index-kVVrq7La.js";const t="files";l("ls /",async({help:e})=>{s.enable("*");const a=await e(t);s(a)});
---
+++ assets/files.test-T9Owwboy.js
@@ -0,0 +1,1 @@
+import{h as l,d as s}from"./test-context-uSkVLoYL.js";import"./index--YbeE7wy.js";const t="files";l("ls /",async({help:e})=>{s.enable("*");const a=await e(t);s(a)});
