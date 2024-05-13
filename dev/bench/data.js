90268a634f7752e31f77092d4ff689f5387a67ef
github-action-benchmark <github@users.noreply.github.com>
2024-05-13 09:50:19 +0000

    add Interpulse Benchmark (benchmarkjs) benchmark result for 6500527bb7e6f6ebc11fbb8aa7a44e2d10b5beb6

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1715593819343,
+  "repoUrl": "https://github.com/dreamcatcher-tech/artifact",
+  "entries": {
+    "Interpulse Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "69952973+inverted-capital@users.noreply.github.com",
+            "name": "inverted-capital",
+            "username": "inverted-capital"
+          },
+          "committer": {
+            "email": "69952973+inverted-capital@users.noreply.github.com",
+            "name": "inverted-capital",
+            "username": "inverted-capital"
+          },
+          "distinct": true,
+          "id": "6500527bb7e6f6ebc11fbb8aa7a44e2d10b5beb6",
+          "message": "ghactions",
+          "timestamp": "2024-05-13T21:49:36+12:00",
+          "tree_id": "ddfb9d5e3532c202ac541965c157ab42178fee73",
+          "url": "https://github.com/dreamcatcher-tech/artifact/commit/6500527bb7e6f6ebc11fbb8aa7a44e2d10b5beb6"
+        },
+        "date": 1715593818805,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "boot",
+            "value": 0.39,
+            "range": "±7.67%",
+            "unit": "ops/sec",
+            "extra": "6 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
